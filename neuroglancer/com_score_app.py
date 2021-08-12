e56cba597abb1d88c38f5d2b34be8a23d4e30202
Eddy O'Donnell <eddyod@yahoo.com>
2021-08-12 07:00:55 -0700

    Moved aglign.score.get to update figure method

--- neuroglancer/com_score_app.py
+++ neuroglancer/com_score_app.py
@@ -5,24 +5,24 @@ import dash_html_components as html
 from django_plotly_dash import DjangoDash
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-alignmentPlot = DjangoDash('AlignmentPlot', external_stylesheets=external_stylesheets)
-
+alignmentPlot = DjangoDash('AlignmentPlot',
+                           external_stylesheets=external_stylesheets)
 align_score = AlignmentScore()
-fig = align_score.get('box_plot')
+
 alignmentPlot.layout = html.Div(children=[
     dcc.Graph(id='plot'),
     html.Label('Select plot types'),
     dcc.RadioItems(id='type',
-        options=[
-            {'label': 'scatter plot', 'value': 'scatter'},
-            {'label': u'box plot', 'value': 'box_plot'},
-        ],
+            options=[
+                {'label': 'scatter plot', 'value': 'scatter'},
+                {'label': u'box plot', 'value': 'box_plot'},
+            ],
         value='scatter'
     ),
 ])
 
 
-@alignmentPlot.callback(
+@alignmentPlot.expanded_callback(
     Output('plot', 'figure'),
     Input('type', 'value'))
 def update_figure(figure_type):
