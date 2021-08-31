2eb5212f65ef1ad4465c0c9fed5f32eef46a6b86
madwilliam <madwilliam5225@gmail.com>
2021-08-31 10:54:47 -0700

    updates

--- neuroglancer/views.py
+++ neuroglancer/views.py
@@ -151,7 +151,7 @@ class Annotations(views.APIView):
         """
         data = []
         layers = LayerData.objects.order_by('prep_id', 'layer', 'input_type_id')\
-            .filter(active=True).filter(input_type_id__in=[1, 3, 5])\
+            .filter(active=True).filter(input_type_id__in=[1, 3, 5,6,7])\
             .filter(layer__isnull=False)\
             .values('prep_id', 'layer','input_type__input_type','input_type_id')\
             .distinct()
