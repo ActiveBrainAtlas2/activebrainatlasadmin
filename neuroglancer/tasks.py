a4d42364a292009cb0689fc462b8f23a21925159
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-04-15 06:26:16 +0700

    Fixing polygon_index isssue

--- neuroglancer/tasks.py
+++ neuroglancer/tasks.py
@@ -2,9 +2,10 @@ import numpy as np
 from statistics import mode
 from django.contrib.auth.models import User
 from django.http import Http404
-from neuroglancer.models import  AnnotationPointArchive, \
-        ArchiveSet, BrainRegion, \
-        PolygonSequence, MarkedCell, StructureCom
+from django.apps import apps
+
+from neuroglancer.models import AnnotationSession,  AnnotationPointArchive, \
+        ArchiveSet, BrainRegion, PolygonSequence
 from brain.models import Animal
 from neuroglancer.bulk_insert import BulkCreateManager
 from neuroglancer.atlas import get_scales
@@ -26,7 +27,8 @@ def update_annotation_data(neuroglancerModel):
     """    
     json_txt = neuroglancerModel.url
     owner_id = neuroglancerModel.owner.id
-
+    #TODO figure out which table to use
+    obj = 'MarkedCell'
     try:
         loggedInUser = User.objects.get(pk=neuroglancerModel.owner.id)
     except User.DoesNotExist:
@@ -44,29 +46,31 @@ def update_annotation_data(neuroglancerModel):
                 label = str(state_layer['name']).strip()
                 if animal is not None and loggedInUser is not None and \
                     label != 'annotation':
-                    inactivate_annotations(animal, label)
+                    inactivate_annotations(obj, animal, label)
                     # move_and_insert_annotations(animal.prep_id, state_layer, owner_id, label, verbose_name="Bulk annotation move and insert",  creator=loggedInUser)
                     # Do not remove these comments.
                     # Uncomment the line below for testing and comment out the line above and the @background
                     # decorator
-                    move_and_insert_annotations(animal.prep_id, state_layer, owner_id, label)
+                    move_and_insert_annotations(obj, animal.prep_id, state_layer, owner_id, label)
 
 
-def inactivate_annotations(animal, label):
+def inactivate_annotations(obj, animal, label):
     """
     Update the existing annotations that are manual, prep, active 
     set them to inactive. Updates are fast, so this does not 
     need to be backgrounded. In fact, it really needs to be done right away.
     """
-    AnnotationPoints.objects.filter(input_type_id=MANUAL)\
-    .filter(animal=animal)\
+    annotation_model = apps.get_model(app_label='neuroglancer', model_name=obj)
+
+    annotation_model.objects\
+    .filter(annotation_session__animal=animal)\
     .filter(active=True)\
     .filter(label=label)\
     .update(active=False)
 
 
-@background(schedule=0)
-def move_and_insert_annotations(prep_id, layer, owner_id, label):
+# @background(schedule=0)
+def move_and_insert_annotations(obj, prep_id, layer, owner_id, label):
     '''
     This is a simple method that just calls two other methods.
     The reason for this method's existence is just to make sure the CPU/time
@@ -76,12 +80,12 @@ def move_and_insert_annotations(prep_id, layer, owner_id, label):
     :param owner_id: int of the auth user primary key
     :param label: char string of the name of the layer
     '''
-    move_annotations(prep_id, owner_id, label)
-    bulk_annotations(prep_id, layer, owner_id, label)
+    move_annotations(obj, prep_id, owner_id, label)
+    bulk_annotations(obj, prep_id, layer, owner_id, label)
 
 
 @background(schedule=0)
-def restore_annotations(archive_id, prep_id, label):
+def restore_annotations(obj, archive_id, prep_id, label):
     '''
         Inactivate the existing annotations that are manual, prep, active.
         It is OK to inactivate them as they must already be in the:
@@ -93,14 +97,17 @@ def restore_annotations(archive_id, prep_id, label):
     :param prep_id: char string of animal name
     :param label: char staring of layer name
     '''
+    annotation_model = apps.get_model(app_label='neuroglancer', model_name=obj)
+    annotation_type = 'STRUCTURE_COM'
     try:
         archive = ArchiveSet.objects.get(pk=archive_id)
     except ArchiveSet.DoesNotExist:
         print('No archive to fetch')
         raise Http404
     
-    AnnotationPoints.objects.filter(input_type_id=MANUAL)\
-    .filter(animal__prep_id=prep_id)\
+
+    annotation_model.objects\
+    .filter(annotation_session__animal__prep_id=prep_id)\
     .filter(active=True)\
     .filter(label=label)\
     .update(active=False)
@@ -108,20 +115,19 @@ def restore_annotations(archive_id, prep_id, label):
     rows = AnnotationPointArchive.objects.filter(archive=archive)
     bulk_mgr = BulkCreateManager(chunk_size=100)
     for row in rows:
-        bulk_mgr.add(AnnotationPoints(animal=row.animal, brain_region=row.brain_region,
-            label=row.label, polygon_id=row.polygon_id, owner=row.owner, input_type=row.input_type,
-            x=row.x, y=row.y, z=row.z))
+        annotation_session = get_annotation_session(annotation_type, row.owner, row.animal, row.brain_region)
+        bulk_mgr.add(annotation_model(annotation_session=annotation_session,
+            label=row.label, x=row.x, y=row.y, z=row.z))
     bulk_mgr.done()
     
     # now delete the existing inactivated rows
-    AnnotationPoints.objects.filter(input_type_id=MANUAL)\
-    .filter(animal__prep_id=prep_id)\
+    annotation_model.objects.filter(annotation_session__animal__prep_id=prep_id)\
     .filter(active=False)\
     .filter(label=label)\
     .delete()
 
     
-def move_annotations(prep_id, owner_id, label):
+def move_annotations(obj, prep_id, owner_id, label):
     '''
     Move existing annotations into the archive. First we get the existing
     rows and then we insert those into the archive table. This is a background
@@ -135,6 +141,9 @@ def move_annotations(prep_id, owner_id, label):
     an appropriate parent in archive_set. After we get the animal,
     we need to create an archive
     '''
+    annotation_model = apps.get_model(app_label='neuroglancer', model_name=obj)
+    annotation_type = 'STRUCTURE_COM'
+
     # animal
     try:
         animal = Animal.objects.get(pk=prep_id)
@@ -162,8 +171,8 @@ def move_annotations(prep_id, owner_id, label):
     start = timer()
     parent = 0
     # check online table
-    rows = AnnotationPoints.objects.filter(input_type__id=MANUAL)\
-        .filter(animal=animal)\
+    rows = annotation_model.objects\
+        .filter(annotation_session__animal=animal)\
         .filter(label=label)
         
     if rows is not None and len(rows) > 0: 
@@ -172,7 +181,6 @@ def move_annotations(prep_id, owner_id, label):
             query_set = AnnotationPointArchive.objects\
                 .filter(animal=animal)\
                 .filter(label=label)\
-                .filter(input_type_id=MANUAL)\
                 .order_by('-id')
         except AnnotationPointArchive.DoesNotExist:
             parent = 0
@@ -182,26 +190,27 @@ def move_annotations(prep_id, owner_id, label):
                 parent = annotationPointArchive.archive.id
             except ArchiveSet.DoesNotExist:
                 parent = 0
-        input_type = InputType.objects.get(pk=MANUAL)
         archive = ArchiveSet.objects.create(parent=parent,
                                             animal=animal,
-                                            input_type=input_type,
                                             label=label,
                                             updatedby=loggedInUser)
         
     bulk_mgr = BulkCreateManager(chunk_size=100)
     for row in rows:
-        bulk_mgr.add(AnnotationPointArchive(animal=row.animal, brain_region=row.brain_region,
-            label=row.label, polygon_id=row.polygon_id, owner=row.owner, input_type=row.input_type, volume_id=row.volume_id,
-            x=row.x, y=row.y, z=row.z, archive=archive, ordering=row.ordering))
+        annotation_session = get_annotation_session(annotation_type, row.owner, row.animal, row.brain_region)
+        bulk_mgr.add(annotation_model(annotation_session=annotation_session,
+            label=row.label, x=row.x, y=row.y, z=row.z))
     bulk_mgr.done()
     rows.delete()
     end = timer()
     print(f'Bulk move took {end - start} seconds') 
 
 
-def bulk_annotations(prep_id, layer, owner_id, label):
+def bulk_annotations(obj, prep_id, layer, owner_id, label):
     start = timer()
+    annotation_model = apps.get_model(app_label='neuroglancer', model_name=obj)
+    annotation_type = 'STRUCTURE_COM'
+
     try:
         loggedInUser = User.objects.get(pk=owner_id)
     except User.DoesNotExist:
@@ -223,19 +232,31 @@ def bulk_annotations(prep_id, layer, owner_id, label):
             x, y, z = annotation.coord * scales
             brain_region = get_brain_region(annotation)
             if brain_region is not None:
-                bulk_mgr.add(AnnotationPoints(animal=animal, brain_region=brain_region,
-                label=label, active=True, owner=loggedInUser, input_type_id=MANUAL,
-                ordering=0,
-                x=x, y=y, z=z))
+                annotation_session = get_annotation_session(annotation_type, loggedInUser, animal, 
+                    brain_region)
+                bulk_mgr.add(annotation_model(annotation_session=annotation_session,
+                    label=label, x=x, y=y, z=z))
+
+                #bulk_mgr.add(annotation_model(animal=animal, brain_region=brain_region,
+                #label=label, active=True, owner=loggedInUser, input_type_id=MANUAL,
+                #ordering=0, x=x, y=y, z=z))
         if annotation._type == 'polygon':
+            annotation_type = 'POLYGON_SEQUENCE'
             polygon_id = annotation.id
             z = mode([ int(np.floor(pointi.coord_start[2]) * z_scale) for pointi in annotation.childs])
             ordering = 1
             for pointi in annotation.childs:
                 xa, ya, _ = pointi.coord_start * scales
-                bulk_mgr.add(AnnotationPoints(animal=animal, brain_region=polygon_structure,
-                owner=loggedInUser, active=True, input_type_id=MANUAL, label=label, polygon_id=polygon_id,
-                x=xa, y=ya, z=z, ordering=ordering))
+
+                annotation_session = get_annotation_session(annotation_type, loggedInUser, animal, 
+                    polygon_structure)
+                bulk_mgr.add(PolygonSequence(annotation_session=annotation_session,
+                    label=label, x=xa, y=ya, z=z, point_order=ordering, polygon_index=polygon_id))
+
+                #bulk_mgr.add(PolygonSequence(animal=animal, brain_region=polygon_structure,
+                #owner=loggedInUser, active=True, input_type_id=MANUAL, label=label, polygon_id=polygon_id,
+                #x=xa, y=ya, z=z, ordering=ordering))
+
                 ordering += 1
         if annotation._type == 'volume':
             volume_id = annotation.id
@@ -245,9 +266,16 @@ def bulk_annotations(prep_id, layer, owner_id, label):
                 z = mode([ int(np.floor(coord.coord_start[2]) * z_scale) for coord in polygoni.childs])
                 for childi in polygoni.childs:
                     xa, ya, _ = childi.coord_start * scales
-                    bulk_mgr.add(AnnotationPoints(animal=animal, brain_region=polygon_structure,
-                    owner=loggedInUser, active=True, input_type_id=MANUAL, label=label, polygon_id=polygon_id,
-                    volume_id=volume_id, x=xa, y=ya, z=z, ordering=ordering))
+
+                    annotation_session = get_annotation_session(annotation_type, loggedInUser, animal, 
+                        brain_region)
+                    bulk_mgr.add(PolygonSequence(annotation_session=annotation_session,
+                        label=label, x=xa, y=ya, z=z, point_order=ordering, polygon_index=polygon_id))
+
+                    #bulk_mgr.add(PolygonSequence(animal=animal, brain_region=polygon_structure,
+                    #owner=loggedInUser, active=True, input_type_id=MANUAL, label=label, polygon_id=polygon_id,
+                    #volume_id=volume_id, x=xa, y=ya, z=z, ordering=ordering))
+                    
                     ordering += 1
     bulk_mgr.done()
     end = timer()
@@ -271,3 +299,22 @@ def get_brain_region(annotation):
         if query_set is not None and len(query_set) > 0:
             brain_region = query_set[0]
     return brain_region
+
+
+def get_annotation_session(annotation_type, annotator, animal, brain_region):
+    query_set = AnnotationSession.objects \
+    .filter(animal=animal)\
+    .filter(brain_region=brain_region)\
+    .filter(annotator=annotator)\
+    .filter(annotation_type=annotation_type)
+
+    if query_set is not None and len(query_set) > 0:
+        annotation_session = query_set[0]
+    else:
+        annotation_session = AnnotationSession.objects.create(\
+            animal=animal,
+            brain_region=brain_region,
+            annotator=annotator,
+            annotation_type=annotation_type
+            )
+    return annotation_session
