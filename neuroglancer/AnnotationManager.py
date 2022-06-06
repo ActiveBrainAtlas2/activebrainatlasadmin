175f91ec3fb59ea8a7632e8dc88e4903a8849865
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-06-06 09:20:57 +0700

    Moved the background task methods back to tasks.py where they belong

--- neuroglancer/AnnotationManager.py
+++ neuroglancer/AnnotationManager.py
@@ -1,14 +1,13 @@
 from attr import has
 import numpy as np
 from statistics import mode
-from django.http import Http404
 from django.apps import apps
-from neuroglancer.models import AnnotationSession,  AnnotationPointArchive, BrainRegion, PolygonSequence,StructureCom,PolygonSequence,MarkedCell,get_region_from_abbreviation
+from neuroglancer.models import AnnotationSession,  AnnotationPointArchive, BrainRegion, \
+    PolygonSequence,StructureCom,PolygonSequence,MarkedCell,get_region_from_abbreviation
 from neuroglancer.bulk_insert import BulkCreateManager
 from neuroglancer.atlas import get_scales
 from neuroglancer.models import MANUAL, POINT_ID, POLYGON_ID,CellType
 from abakit.lib.annotation_layer import AnnotationLayer,Annotation
-from background_task import background
 from neuroglancer.AnnotationBase import AnnotationBase
 
 class AnnotationManager(AnnotationBase):
@@ -67,41 +66,6 @@ class AnnotationManager(AnnotationBase):
                     self.update_data_in_current_layer()
 
 
-    # @background(schedule=0)
-    def archive_and_insert_annotations(self):
-        """The main function that updates the database with annotations in the current_layer attribute
-           This function loops each annotation in the curent layer and inserts/archive points in the 
-           appropriate table
-        """
-        marked_cells = []
-        for annotationi in self.current_layer.annotations:
-            if annotationi.is_com():
-                brain_region = get_region_from_abbreviation(annotationi.get_description())
-                new_session = self.get_new_session_and_archive_points(brain_region=brain_region,annotation_type='STRUCTURE_COM')
-                self.add_com(annotationi,new_session)
-            if annotationi.is_cell():
-                marked_cells.append(annotationi)
-            if annotationi.is_polygon():
-                brain_region = get_region_from_abbreviation('polygon')
-                new_session = self.get_new_session_and_archive_points(brain_region=brain_region,annotation_type='POLYGON_SEQUENCE')
-                self.add_polygons(annotationi,new_session)
-            if annotationi.is_volume():
-                brain_region = get_region_from_abbreviation(annotationi.get_description())
-                new_session = self.get_new_session_and_archive_points(brain_region=brain_region,annotation_type='POLYGON_SEQUENCE')
-                self.add_volumes(annotationi,new_session)
-        if len(marked_cells)>0:
-            categories = np.array([i.category for i in marked_cells])
-            unique_category = np.unique(categories)
-            for category in unique_category:
-                in_category = categories == category
-                cells = marked_cells[in_category]
-                new_session = self.get_new_session_and_archive_points(brain_region=brain_region,annotation_type='MARKED_CELL')
-                for annotationi in cells:
-                    cell_type = CellType.objects.filter(cell_type = category).first()
-                    if cell_type is not None:
-                        brain_region = get_region_from_abbreviation('point')
-                        self.add_marked_cells(annotationi,new_session,cell_type)
-        self.bulk_mgr.done()
     
     def is_structure_com(self,annotationi:Annotation):
         """ determines if a point annotation is a structure COM
@@ -235,31 +199,3 @@ class AnnotationManager(AnnotationBase):
                                 annotation_type=annotation_type,parent = parent)
         return annotation_session
     
-@background(schedule=0)
-def restore_annotations(session_id):
-    '''
-        Inactivate the existing annotations that are manual, prep, active.
-        It is OK to inactivate them as they must already be in the:
-        annotation_points_archive table for this operation to be
-        accessible. 
-        Fetch rows from the archive and 
-        insert into online table
-    :archive_id: int of primary key of archive_set
-    :param prep_id: char string of animal name
-    :param label: char staring of layer name
-    '''
-    try:
-        annotation_session = AnnotationSession.objects.get(pk=session_id)
-    except AnnotationSession.DoesNotExist:
-        print('No annotation_session to fetch')
-        raise Http404
-    data_model = annotation_session.get_session_model()
-    data_model.objects.filter(annotation_session=annotation_session).delete()
-    field_names = [f.name for f in data_model._meta.get_fields() if not f.name == 'id']
-    rows = AnnotationPointArchive.objects.filter(annotation_session=annotation_session)
-    bulk_mgr = BulkCreateManager(chunk_size=100)
-    for row in rows:
-        fields = [getattr(row,namei) for namei in field_names]
-        input = dict(zip(field_names,fields))
-        bulk_mgr.add(data_model(**input))
-    bulk_mgr.done()
