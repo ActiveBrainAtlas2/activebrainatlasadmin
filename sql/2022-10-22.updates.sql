5e59df78e0852ed5ee58a121ddb02c4271f454ff
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-10-21 15:50:42 -0400

    Removed indexes from annotation manager and replaced them with first in the queryset

--- sql/2022-10-22.updates.sql
+++ sql/2022-10-22.updates.sql
@@ -1,49 +1,55 @@
 truncate marked_cells;
-select * from marked_cells mc; 
+SELECT `annotation_session`.`active`, `annotation_session`.`created`, `annotation_session`.`id`, 
+`annotation_session`.`FK_prep_id`, `annotation_session`.`FK_structure_id`, `annotation_session`.`FK_annotator_id`, 
+`annotation_session`.`annotation_type`, `annotation_session`.`FK_parent` 
+FROM `annotation_session` 
+WHERE (`annotation_session`.`FK_prep_id` = 'DK41' 
+AND `annotation_session`.`FK_structure_id` = 52 
+AND `annotation_session`.`FK_annotator_id` = 38 
+AND `annotation_session`.`annotation_type` = 'MARKED_CELL' AND `annotation_session`.`active`);
 
-select au.username ,s.*, as2.*
+
+select as2.id, mc.source, ct.cell_type 
 from marked_cells mc
 inner join annotation_session as2 on mc.FK_session_id = as2.id 
-inner join auth_user au on as2.FK_annotator_id = au.id 
-inner join structure s on as2.FK_structure_id = s.id 
-where (mc.FK_cell_type_id is null OR as2.);
+inner join cell_type ct on mc.FK_cell_type_id = ct.id 
+where as2.FK_prep_id = 'DK41' 
+and as2.FK_structure_id = 52
+and as2.FK_annotator_id = 38
+and as2.annotation_type = 'MARKED_CELL'
+and mc.source = 'HUMAN_NEGATIVE' and ct.cell_type  = 'mixed'
+order by mc.source, ct.cell_type; 
 
-desc marked_cells;
+SELECT `marked_cells`.`FK_session_id`, `marked_cells`.`source`, `marked_cells`.`FK_cell_type_id` 
+FROM `marked_cells` 
+INNER JOIN `annotation_session` ON (`marked_cells`.`FK_session_id` = `annotation_session`.`id`) 
+INNER JOIN `cell_type` ON (`marked_cells`.`FK_cell_type_id` = `cell_type`.`id`) 
+WHERE (`annotation_session`.`FK_prep_id` = 'DK41' 
+AND `annotation_session`.`FK_structure_id` = 52 
+AND `annotation_session`.`FK_annotator_id` = 38 
+AND `annotation_session`.`annotation_type` = 'MARKED_CELL' 
+AND `annotation_session`.`active` AND `cell_type`.`cell_type` = 'mixed');
+
+select mc.FK_session_id, mc.source, ct.cell_type, as2.annotation_type , as2.FK_parent  
+from marked_cells mc 
+inner join annotation_session as2 on mc.FK_session_id = as2.id 
+left join cell_type ct on mc.FK_cell_type_id = ct.id 
+order by as2.created;
+
+select * from marked_cells mc; 
+
+
+
+-- marked cells modification
 alter table marked_cells modify column source varchar(25);
 update marked_cells set source='UNMARKED' where source = 'NULL';
 alter table marked_cells modify column source enum('MACHINE_SURE','MACHINE_UNSURE','HUMAN_POSITIVE','HUMAN_NEGATIVE','UNMARKED') NOT NULL;
 update marked_cells set FK_cell_type_id = 26 where FK_cell_type_id is null;
-
-ALTER TABLE `annotation_session` ADD INDEX `K__active` (`active`);
-ALTER TABLE `annotation_session` ADD INDEX `K__annotation_type` (`annotation_type`);
+-- annotation session 
 ALTER TABLE `annotation_session` ADD INDEX `K__annotation_type_active` (`active`, `annotation_type`);
-
-explain SELECT id, FK_prep_id, FK_structure_id, 
-FK_annotator_id, created, annotation_type, 
-FK_parent, active 
-FROM annotation_session
-WHERE active = 1 
-AND annotation_type = 'MARKED_CELL'
-ORDER BY 
-FK_prep_id ASC, 
-annotation_session.annotation_type ASC, 
-annotation_session.FK_annotator_id ASC
-;
-show create table annotation_session;
-CREATE TABLE annotation_session (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  created timestamp NOT NULL DEFAULT current_timestamp(),
-  annotation_type enum('POLYGON_SEQUENCE','MARKED_CELL','STRUCTURE_COM') DEFAULT NULL,
-  FK_annotator_id int(11) NOT NULL,
-  FK_prep_id varchar(20) NOT NULL,
-  FK_parent int(11) NOT NULL COMMENT 'SELF-REFERENCES id [IN THIS TABLE]',
-  FK_structure_id int(11) NOT NULL COMMENT 'TABLE structure SHOULD ONLY CONTAIN BIOLOGICAL STRUCTURES',
-  active tinyint(1) DEFAULT 0,
-  PRIMARY KEY (id),
-  KEY FK_annotator_id (FK_annotator_id),
-  KEY FK_prep_id (FK_prep_id),
-  KEY FK_structure_id (FK_structure_id),
-  CONSTRAINT annotation_session_ibfk_1 FOREIGN KEY (FK_annotator_id) REFERENCES auth_user (id),
-  CONSTRAINT annotation_session_ibfk_2 FOREIGN KEY (FK_prep_id) REFERENCES animal (prep_id),
-  CONSTRAINT annotation_session_ibfk_3 FOREIGN KEY (FK_structure_id) REFERENCES structure (id)
-) ENGINE=InnoDB AUTO_INCREMENT=6983 DEFAULT CHARSET=utf8mb3
+-- finish updates
+select * from annotation_session as2 where id = 4058;
+desc animal; 
+truncate table background_task;
+truncate table background_task_completedtask; 
+truncate table marked_cells; 
