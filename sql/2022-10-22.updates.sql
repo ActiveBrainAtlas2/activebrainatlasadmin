810c5f3fac998ea45e2795ef35ae083819c4a704
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-12-01 10:49:48 -0500

    Annotation saving, archiving and restoring has been redone.

--- sql/2022-10-22.updates.sql
+++
@@ -1,9 +0,0 @@
-desc marked_cells; 
--- marked cells modification
-alter table marked_cells modify column source varchar(25);
-update marked_cells set source='UNMARKED' where source = 'NULL';
-alter table marked_cells modify column source enum('MACHINE_SURE','MACHINE_UNSURE','HUMAN_POSITIVE','HUMAN_NEGATIVE','UNMARKED') NOT NULL;
-update marked_cells set FK_cell_type_id = 26 where FK_cell_type_id is null;
--- annotation session 
-ALTER TABLE `annotation_session` ADD INDEX `K__annotation_type_active` (`active`, `annotation_type`);
--- finish updates
